3b11d2e8eef038741cefaee5adcdba6967473cd9
Mike Jones <mikejonestechno@users.noreply.github.com>
2024-01-04 15:00:57 +1100

    Draft YoY forecast chart (#40)

--- notebooks/temp.py
+++ notebooks/temp.py
@@ -1,1 +1,4 @@
-print("Hello, temp World!")
+# SonarLint extension for VSCode supports Python and iPython Notebooks
+# SonarQube and SonarCloud support Python but NOT iPython Notebooks
+# This file was created to test SonarLint and SonarQube/SonarCloud could find and scan a python file
+print("Hello, World!")
