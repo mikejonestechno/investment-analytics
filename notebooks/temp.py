c13a351f9b084b9a1a6125bcf0bccc5ceb08b356
mikejonestechno <mikejonestechno@users.noreply.github.com>
2024-01-11 00:46:29 +0000

    Use mock and stubbed data for test

--- notebooks/temp.py
+++
@@ -1,4 +0,0 @@
-# SonarLint extension for VSCode supports Python and iPython Notebooks
-# SonarQube and SonarCloud support Python but NOT iPython Notebooks
-# This file was created to test SonarLint and SonarQube/SonarCloud could find and scan a python file
-print("Hello, World!")
