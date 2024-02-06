bd7e34357c3f991359a686099551402602bbefe8
Mike Jones <mikejonestechno@users.noreply.github.com>
2024-02-06 17:40:33 +1100

    Refactor x_years to x_ticks in Chart class constructor and base_chart method

--- src/chart_format.py
+++ src/chart_format.py
@@ -5,14 +5,14 @@ import pandas as pd
 
 
 class Chart:
-    def __init__(self, chart_title='', chart_source='', y_label='', x_label='Date', x_years=5):
+    def __init__(self, chart_title='', chart_source='', y_label='', x_label='Date', x_ticks=5):
         self.chart_title = chart_title
         self.chart_source = chart_source
         self.y_label = y_label
         self.x_label = x_label
-        self.x_years = x_years
+        self.x_ticks = x_ticks
 
-    def basic_chart(self, df: pd.DataFrame):
+    def base_chart(self, df: pd.DataFrame):
 
         plt.figure(figsize=(10, 6))
         plt.title(self.chart_title)
@@ -25,12 +25,12 @@ class Chart:
 
         ax = plt.gca()  # Get the current Axes instance
         plt.xlabel(self.x_label)
-        start_year = (df.index[0].year // self.x_years) * self.x_years
-        end_year = (df.index[-1].year // self.x_years) * self.x_years + self.x_years
+        start_year = (df.index[0].year // self.x_ticks) * self.x_ticks
+        end_year = (df.index[-1].year // self.x_ticks) * self.x_ticks + self.x_ticks
         left_limit = pd.to_datetime(f'{start_year}-01-01')
         right_limit = pd.to_datetime(f'{end_year}-01-01')
         plt.xlim(left=left_limit, right=right_limit)    
-        ax.xaxis.set_major_locator(mdates.YearLocator(self.x_years))  # Set major ticks every x years
+        ax.xaxis.set_major_locator(mdates.YearLocator(self.x_ticks))  # Set major ticks every x years
         ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))  # Format major ticks as years
 
         plt.ylabel(self.y_label)
