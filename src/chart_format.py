9f2c3c205c546e12d35d6392e443e968ca0137ea
Mike Jones <mikejonestechno@users.noreply.github.com>
2024-02-09 12:21:07 +1100

    Update index.md and retirement.ipynb, and refactor chart_format.py

--- src/chart_format.py
+++ src/chart_format.py
@@ -4,7 +4,7 @@ import matplotlib.dates as mdates
 import pandas as pd
 import numpy as np
 
-class Chart:
+class BaseChart:
     def __init__(self, chart_title='', chart_source='', chart_author='right', y_label='', y_ticks=0, y_log=False, y_log_ticks=None, x_label='Date', x_ticks=5, data_column='column_name'):
         self.chart_title = chart_title
         self.chart_source = chart_source
@@ -43,22 +43,44 @@ class Chart:
         ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))  # Format major ticks as years
 
         plt.ylabel(self.y_label)
-        if self.y_log:
-            y_ticks_labels = np.array([str(f'{ytick:.0f}') for ytick in self.y_log_ticks])
-            bottom_limit = self.y_log_ticks[0]
-            top_limit = self.y_log_ticks[-1]
-            plt.yscale('log')
-            plt.ylim(bottom_limit, top_limit)
-            plt.yticks(self.y_log_ticks, y_ticks_labels)
+
+        return plt, colors
+    
+
+class LogChart(BaseChart):
+    def __init__(self, y_log_ticks=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.y_log_ticks = np.array(y_log_ticks)
+
+    def base_chart(self, df: pd.DataFrame):
+        plt, colors = super().base_chart(df)
+
+        """ Logarithmic y-axis chart """
+        y_ticks_labels = np.array([str(f'{ytick:.0f}') for ytick in self.y_log_ticks])
+        bottom_limit = self.y_log_ticks[0]
+        top_limit = self.y_log_ticks[-1]
+        plt.yscale('log')
+        plt.ylim(bottom_limit, top_limit)
+        plt.yticks(self.y_log_ticks, y_ticks_labels)
+
+        return plt, colors
+
+class StandardChart(BaseChart):
+    def __init__(self, y_ticks=100, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.y_ticks = y_ticks
+
+    def base_chart(self, df: pd.DataFrame):
+        plt, colors = super().base_chart(df)
+        
+        """ Standard y-axis chart """
+        self.y_ticks = self.y_ticks if self.y_ticks else 100
+        if df[self.data_column].min() > 0:
+            bottom_limit = 0
         else:
-            """ if we have a data_column set y-axis limit as multiples of y_ticks """
-            self.y_ticks = self.y_ticks if self.y_ticks else 100
-            if df[self.data_column].min() > 0:
-                bottom_limit = 0
-            else:
-                bottom_limit = (df[self.data_column].min() // self.y_ticks) * self.y_ticks    
-            top_limit = (df[self.data_column].max() // self.y_ticks) * self.y_ticks + self.y_ticks
-            plt.yticks(np.arange(bottom_limit, top_limit + 1, self.y_ticks))
-            plt.ylim(bottom_limit, top_limit)
+            bottom_limit = (df[self.data_column].min() // self.y_ticks) * self.y_ticks    
+        top_limit = (df[self.data_column].max() // self.y_ticks) * self.y_ticks + self.y_ticks
+        plt.yticks(np.arange(bottom_limit, top_limit + 1, self.y_ticks))
+        plt.ylim(bottom_limit, top_limit)
 
         return plt, colors
