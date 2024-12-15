d4ec7aee56d40bc5868cff6535e3fb59c72a5f75
Mike Jones <mikejonestechno@users.noreply.github.com>
2024-12-15 18:28:52 +1100

    Adjust fig size

--- src/chart_format.py
+++ src/chart_format.py
@@ -1,3 +1,4 @@
+from matplotlib import layout_engine
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import matplotlib.dates as mdates
@@ -26,9 +27,8 @@ class BaseChart:
         self.data_column = data_column
 
     def base_chart(self, df: pd.DataFrame):
-        px = 1/plt.rcParams['figure.dpi']  # pixel in inches https://matplotlib.org/stable/gallery/subplots_axes_and_figures/figure_size_units.html
-        #plt.figure(figsize=(10, 6))
-        plt.figure(figsize=(1024*px, 640*px))
+        plt.figure(figsize=(10, 6))
+        plt.rcParams['figure.autolayout'] = True
         plt.title(self.chart_title)
 
         colors = list(plt.rcParams['axes.prop_cycle'])
